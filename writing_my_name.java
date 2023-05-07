e79ff657788797b1b11ed53b7bdfe968e2fd457d
shubhamK010 <shubhamkmb@gmail.com>
2023-05-07 12:29:05 +0530

    Create writing_my_name.java

---
+++ writing_my_name.java
@@ -0,0 +1,11 @@
+package self_practice;
+
+public class writing_my_name {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	System.out.println("My name is Shubham");	
+	}
+
+}
